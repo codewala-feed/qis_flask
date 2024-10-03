49c82ab699c603fbc054ea9677dbb49274b4a822
nikhil <nikhilpokuri143@gmail.com>
2024-10-03 15:54:38 +0530

    ece4 calculator

---
+++ ece4/calci/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def homepage():
+    return "Hey coder, you are in homepage"
+
+@app.route("/wish", methods=["GET"])
+def wishpage():
+    return "We wish you happy coding :)"
+
+@app.route("/project", methods=["GET", "POST"])
+def calculator():
+    if request.method == "POST":
+        num1 = int(request.form["num1"])
+        num2 = int(request.form["num2"])
+        opr = request.form["opr"]
+
+        if opr == "add":
+            output = f"{num1} + {num2} = {num1+num2}"
+            return render_template("index.html", data=output)
+        elif opr == "sub":
+            output = f"{num1} - {num2} = {num1-num2}"
+            return render_template("index.html", data=output) 
+        elif opr == "mul":
+            output = f"{num1} x {num2} = {num1*num2}"
+            return render_template("index.html", data=output) 
+        elif opr == "div":
+            output = f"{num1} / {num2} = {num1/num2}"
+            return render_template("index.html", data=output) 
+        else:
+            return "Invalid operation"
+    else:
+        return render_template("index.html")
