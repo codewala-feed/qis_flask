d88052013e43f0216dd2f252f7cb7fdc10755bce
nikhil <nikhilpokuri143@gmail.com>
2024-10-08 10:16:54 +0530

    csit_bs project

--- csit_bs/calci_project/app.py
+++ csit_bs/calci_project/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+my_client = MongoClient("localhost", 27017)
+my_db = my_client["csit_bs_calci"]
+results = my_db["results"]
+
 
 @app.route("/", methods=["GET"])
 def homepage():
@@ -15,16 +21,32 @@ def calculator():
 
         if opr == "add":
             output = f"{num1} + {num2} = {num1+num2}"
+            results.insert_one(
+                {"num1":num1, "num2":num2, "opr":opr, "result":num1+num2}
+            )
             return render_template("index.html", data=output)
         elif opr == "sub":
             output = f"{num1} - {num2} = {num1-num2}"
+            results.insert_one(
+                {"num1":num1, "num2":num2, "opr":opr, "result":num1-num2}
+            )
             return render_template("index.html", data=output) 
         elif opr == "mul":
             output = f"{num1} x {num2} = {num1*num2}"
+            results.insert_one(
+                {"num1":num1, "num2":num2, "opr":opr, "result":num1*num2}
+            )
             return render_template("index.html", data=output) 
         elif opr == "div":
-            output = f"{num1} / {num2} = {num1/num2}"
-            return render_template("index.html", data=output) 
+            try:
+                output = f"{num1} / {num2} = {num1/num2}"
+                results.insert_one(
+                {"num1":num1, "num2":num2, "opr":opr, "result":num1/num2}
+            )
+                return render_template("index.html", data=output)
+            except Exception:
+                error = "Please change num2 as non-zero"
+                return  render_template("index.html", data=error)
         else:
             return "Invalid Operation"
     else:
--- csit_bs/calci_project/static/js/style.js
+++ csit_bs/calci_project/static/js/style.js
@@ -1,3 +1,8 @@
-function entered(){
-    alert('Working')
-}
+const btn1 = document.getElementById("btn1")
+btn1.addEventListener("mousedown", function(){
+    btn1.style.boxShadow = "10px 10px 10px brown";
+})
+
+btn1.addEventListener("mouseup", function(){
+    btn1.style.boxShadow = "";
+})
