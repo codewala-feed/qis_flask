4dbdf501d07dbbe2a8e4f8988b5664e383ca343f
nikhil <nikhilpokuri143@gmail.com>
2024-10-19 11:57:46 +0530

    calci

--- ece3/calci/app.py
+++ ece3/calci/app.py
@@ -2,28 +2,22 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-my_client = MongoClient("localhost", 27017)
-my_db = my_client["ece3_calci"] # database
+# get the connection info from bytexl mongodb nimbus
+host = "ocdb.app"
+port = 5050
+database = "" # your database
+username = "" # your username
+password = "" # your password
+
+connection_str = f"mongodb://{username}:{password}@{host}:{port}/{database}"
+
+my_client = MongoClient(connection_str)
+my_db = my_client[database] # database
 results = my_db["results"] # collection
 
 @app.route("/", methods=["GET"])
 def homepage():
     return "<h1>Hey coder, welcome to homepage</h1>"
-
-@app.route("/calci/<opr>/<num1>/<num2>", methods=["GET"])
-def calci(opr, num1, num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    if opr == "add":
-        return f"<h1>[CALCI]: {num1} + {num2} = {num1 + num2}</h1>"
-    elif opr == "sub":
-        return f"<h1>[CALCI]: {num1} - {num2} = {num1 - num2}</h1>"
-    elif opr == "mul":
-        return f"<h1>[CALCI]: {num1} X {num2} = {num1 * num2}</h1>"
-    elif opr == "div":
-        return f"<h1>[CALCI]: {num1} / {num2} = {num1 / num2}</h1>"
-    else:
-        return "Invalid Inputs, please enter valid operator and nums"
 
 @app.route("/project", methods=["GET", "POST"])
 def calculator():
