904b2253264ba3d7f9f61c18f50febf1bf00cc39
nikhil <nikhilpokuri143@gmail.com>
2024-10-26 10:14:12 +0530

    login calci

--- ece4/calci/app.py
+++ ece4/calci/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -12,53 +12,65 @@ password = "" # your password
 connection_string = f"mongodb://{username}:{password}@{host}:{port}/{database}"
 
 my_client = MongoClient(connection_string)
-my_db = my_client[database] #database
+my_db = my_client[database] # your database
 results = my_db["results"] # collection
+
+isLoggedIn = False
 
 @app.route("/", methods=["GET"])
 def homepage():
-    return "Hey coder, you are in homepage"
+    return render_template("login.html")
 
-@app.route("/wish", methods=["GET"])
-def wishpage():
-    return "We wish you happy coding :)"
+@app.route("/login-details", methods=["POST"])
+def get_login():
+    global isLoggedIn
+    email = request.form["email"]
+    password = request.form["password"]
+    if email == "admin@gmail.com" and password == "Admin#123":
+        isLoggedIn = True
+        return redirect("/project")
+    else:
+        return redirect("/")
 
 @app.route("/project", methods=["GET", "POST"])
 def calculator():
-    if request.method == "POST":
-        num1 = int(request.form["num1"])
-        num2 = int(request.form["num2"])
-        opr = request.form["opr"]
+    if isLoggedIn == True:
+        if request.method == "POST":
+            num1 = int(request.form["num1"])
+            num2 = int(request.form["num2"])
+            opr = request.form["opr"]
 
-        if opr == "add":
-            output = f"{num1} + {num2} = {num1+num2}"
-            results.insert_one(
-                {"n1":num1, "n2":num2, "operation":opr, "result":num1+num2}
-            )
-            return render_template("index.html", data=output)
-        elif opr == "sub":
-            output = f"{num1} - {num2} = {num1-num2}"
-            results.insert_one(
-                {"n1":num1, "n2":num2, "operation":opr, "result":num1-num2}
-            )
-            return render_template("index.html", data=output) 
-        elif opr == "mul":
-            output = f"{num1} x {num2} = {num1*num2}"
-            results.insert_one(
-                {"n1":num1, "n2":num2, "operation":opr, "result":num1*num2}
-            )
-            return render_template("index.html", data=output) 
-        elif opr == "div":
-            if num2 != 0:
-                output = f"{num1} / {num2} = {num1/num2}"
+            if opr == "add":
+                output = f"{num1} + {num2} = {num1+num2}"
                 results.insert_one(
-                {"n1":num1, "n2":num2, "operation":opr, "result":num1/num2}
-            )
+                    {"n1":num1, "n2":num2, "operation":opr, "result":num1+num2}
+                )
+                return render_template("index.html", data=output)
+            elif opr == "sub":
+                output = f"{num1} - {num2} = {num1-num2}"
+                results.insert_one(
+                    {"n1":num1, "n2":num2, "operation":opr, "result":num1-num2}
+                )
                 return render_template("index.html", data=output) 
+            elif opr == "mul":
+                output = f"{num1} x {num2} = {num1*num2}"
+                results.insert_one(
+                    {"n1":num1, "n2":num2, "operation":opr, "result":num1*num2}
+                )
+                return render_template("index.html", data=output) 
+            elif opr == "div":
+                if num2 != 0:
+                    output = f"{num1} / {num2} = {num1/num2}"
+                    results.insert_one(
+                    {"n1":num1, "n2":num2, "operation":opr, "result":num1/num2}
+                )
+                    return render_template("index.html", data=output) 
+                else:
+                    error = "Please change num2 as non-zero"
+                    return render_template("index.html", data=error)
             else:
-                error = "Please change num2 as non-zero"
-                return render_template("index.html", data=error)
+                return "Invalid operation"
         else:
-            return "Invalid operation"
+            return render_template("index.html")
     else:
-        return render_template("index.html")
+        return redirect("/")
